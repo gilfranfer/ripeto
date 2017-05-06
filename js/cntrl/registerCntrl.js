98f7ef771206451718e897fd03c734303a3bb060
Fernando Gil <gil.franfer@gmail.com>
2017-05-06 14:18:47 -0500

    Initial Setup
    Adding Index Page with Login and link to Register form.
    Task #13, #14 and #15
    - RIPETO ITERATION 0

---
+++ js/cntrl/registerCntrl.js
@@ -0,0 +1,11 @@
+ripetoApp.controller('RegisterCntrl',['$scope', 
+	function($scope){
+		$scope.message = "Welcome to my app";
+	}
+]);
+
+ripetoApp.controller('SuccessCntrl',['$scope',
+	function($scope){
+		
+	}
+]);
---
+++ js/ripeto-app.js
@@ -0,0 +1,33 @@
+var ripetoApp = angular.module('ripetoApp',['ngRoute']);
+
+/*
+Basic controller example:
+ripeto.controller('registerCtrl', [
+	'$scope', 
+	function($scope){
+		$scope.message = "Welcome to Ripeto";
+	}
+] );
+<div ng-controller="registerCtrl">
+*/
+
+ripetoApp.config(['$routeProvider',
+	function($routeProvider){
+		$routeProvider.
+			when('/login',{
+				templateUrl: 'views/login.html',
+				controller: 'RegisterCntrl'
+			}).
+			when('/register',{
+				templateUrl: 'views/register.html',
+				controller: 'RegisterCntrl'
+			}).
+			when('/success', {
+				templateUrl: 'views/success.html',
+				controller: 'SuccessCntrl'
+			}).
+			otherwise({
+				redirectTo: 'login'
+			});
+	}
+]);
