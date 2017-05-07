d0202ad152fd3b5cd0152eeaded3bb36b1db8b07
Fernando Gil <gil.franfer@gmail.com>
2017-05-07 17:57:10 -0500

    Saving User Data
    Persist User Data to Firebase

--- js/svc/authenticationSvc.js
+++ js/svc/authenticationSvc.js
@@ -1,22 +1,40 @@
 ripetoApp.factory( 'AuthenticationSvc', ['$rootScope', '$firebaseAuth',
+	
 	function($rootScope, $firebaseAuth){
+		
 		var ref = firebase.database().ref();
 		var auth = $firebaseAuth();
+		var users_folder = 'users';
 
 		return{
 			login: function(user){
-				$rootScope.message = "Welcome " +user.email;
+				$rootScope.message = "Welcome " + user.email;
 			},
-			register:  function(user){
+			register: function(user){
 				auth.$createUserWithEmailAndPassword(
 						user.email, user.pwd
-				).then( function(regUser){
+				).then(
+				/* Use firebase uid from the record created to
+				 * save more user details in our users folder */ 
+				function(regUser){
+					console.log(user);
+					console.log(firebase.database.ServerValue.TIMESTAMP);
+					var regRef = ref.child(users_folder).child(regUser.uid).set({
+						firstname: user.firstname,
+						lastname: user.lastname,
+						email: user.email,
+						uderid: regUser.uid,
+						date: firebase.database.ServerValue.TIMESTAMP						
+					});
+
 					$rootScope.message = user.firstname + 
 						" you are now part of the awesomeness";
-				} ).catch( function(error){
+				} 
+				).catch( function(error){
 					$rootScope.message = error.message;
 				});
 			}
 		};//return
 	}
+
 ]);
