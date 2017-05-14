afd5ca66348392de9b7fbd29f5b8a781c96d7770
Fernando Gil <gil.franfer@gmail.com>
2017-05-14 14:32:33 -0500

    Saving Activities
    Adding a simple input and button to Save new Activities to Firebase

--- js/cntrl/controllers.js
+++ js/cntrl/controllers.js
@@ -17,11 +17,35 @@ ripetoApp.controller('AuthenticationCntrl',
 	}]//function
 );//controller
 
-ripetoApp.controller('HomeCntrl',['$scope',
-	function($scope){
+ripetoApp.controller('HomeCntrl',
+	['$scope', '$firebaseAuth', '$firebaseArray',
+	function($scope, $firebaseAuth, $firebaseArray){
+
+		var ref = firebase.database().ref();
+		var auth = $firebaseAuth();
+		
+		auth.$onAuthStateChanged( function(user){
+    		if(user){
+				var allActivities = $firebaseArray(
+							ref.child('users').child(user.uid).child('activities')
+						);
+				
+				$scope.addActivity = function(){
+					console.log("activities:");
+					console.log(allActivities);
+					allActivities.$add({
+						name: $scope.activityName,
+						date: firebase.database.ServerValue.TIMESTAMP
+					}).then( function(){
+						$scope.activityName = '';
+					});
+				};
+    		
+			}
+		}); //onAuthStateChanged
 
 	}
-]);
+]);//controller
 
 ripetoApp.controller('ErrorCntrl',['$scope',
 	function($scope){
